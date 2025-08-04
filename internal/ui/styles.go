795e839887f520132a5f821641f2eb307c2d8527
vybraan <ailtonbauque@outlook.com>
2025-08-05 00:41:56 +0200

    feat(theme): define Styles struct and extend Theme interface

--- internal/ui/styles.go
+++ internal/ui/styles.go
@@ -13,7 +13,33 @@ var (
 	once          sync.Once
 )
 
+type Styles struct {
+	Base         lipgloss.Style
+	SelectedBase lipgloss.Style
+
+	Title        lipgloss.Style
+	Subtitle     lipgloss.Style
+	Text         lipgloss.Style
+	TextSelected lipgloss.Style
+	Muted        lipgloss.Style
+	Subtle       lipgloss.Style
+
+	Success lipgloss.Style
+	Error   lipgloss.Style
+	Warning lipgloss.Style
+	Info    lipgloss.Style
+
+	// Inputs
+	TextArea textarea.Styles
+}
+
 type Theme struct {
+	Name   string
+	IsDark bool
+
+
+	Styles *Styles
+
 	DocStyle            lipgloss.Style
 	SpinnerStyle        lipgloss.Style
 	StatusBar           lipgloss.Style
@@ -29,9 +55,13 @@ type Theme struct {
 }
 
 func NewDefaultTheme() Theme {
+
+	base := lipgloss.NewStyle().
+		Foreground(lipgloss.Color("#D9DCCF"))
+
 	statusBar := lipgloss.NewStyle().
-		Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B27F"}).
-		Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
+		Foreground(lipgloss.Color("#C1C6B27F")).
+		Background(lipgloss.Color("#353533"))
 
 	return Theme{
 		DocStyle:     lipgloss.NewStyle().Margin(1, 2),
@@ -71,16 +101,45 @@ func NewDefaultTheme() Theme {
 		BottomModelTxt: lipgloss.NewStyle().Inherit(statusBar),
 
 		ViewportStyleNormal: lipgloss.NewStyle().
-			BorderBottom(true).
+			BorderBottom(false).
 			BorderStyle(lipgloss.RoundedBorder()).
 			BorderBottomForeground(lipgloss.Color("#7aa2f7")),
 
 		ViewportStyleInsert: lipgloss.NewStyle().
-			BorderBottom(true).
+			BorderBottom(false).
 			BorderStyle(lipgloss.RoundedBorder()).
 			BorderBottomForeground(lipgloss.Color("#c3e88d")),
 
 		SenderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		Styles: &Styles{
+			Base:  base,
+			Muted: base.Foreground(lipgloss.Color("#858392")),
+			TextArea: textarea.Styles{
+				Focused: textarea.StyleState{
+					Base:             base,
+					Text:             base,
+					LineNumber:       base.Foreground(lipgloss.Color("#605F6B")),
+					CursorLine:       base,
+					CursorLineNumber: base.Foreground(lipgloss.Color("#605F6B")),
+					Placeholder:      base.Foreground(lipgloss.Color("#605F6B")),
+					Prompt:           base.Foreground(lipgloss.Color("#68FFD6")),
+				},
+				Blurred: textarea.StyleState{
+					Base:             base,
+					Text:             base.Foreground(lipgloss.Color("#858392")),
+					LineNumber:       base.Foreground(lipgloss.Color("#858392")),
+					CursorLine:       base,
+					CursorLineNumber: base.Foreground(lipgloss.Color("#858392")),
+					Placeholder:      base.Foreground(lipgloss.Color("#605F6B")),
+					Prompt:           base.Foreground(lipgloss.Color("#858392")),
+				},
+				Cursor: textarea.CursorStyle{
+					Color: lipgloss.Color("#c3e88d"),
+					Shape: tea.CursorBar,
+					Blink: true,
+				},
+			},
+		},
 	}
 }
 
